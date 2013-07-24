82e05c126b2df493b54e940efd10cd87f7363992
Fabian <fabian@peersquared.info>
2013-07-25 00:42:26 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,6 +1,6 @@
 <?php 
 require('../Pusher.php');
-$pusher = new Pusher('ce5e15ae4b2ea6d71902', '24e3586ec6834d116000', '42177');
+$pusher = new Pusher($key, $secret, $app_id);
 
 $user_id = rand(1,4000);
 $user_info = array('name' => $_GET["your_name"]);
