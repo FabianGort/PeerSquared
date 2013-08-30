661a328c99f4cff1755d1b0941737e50778199b8
Fabian <fabian@peersquared.info>
2013-08-30 11:34:42 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -6,4 +6,5 @@ $pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
 $user_id = rand(1,4000);
 $user_info = array('name' => $_GET["your_name"]);
 echo $pusher->presence_auth($_POST['channel_name'], $_POST['socket_id'], $user_id, $user_info);
+
 ?>
