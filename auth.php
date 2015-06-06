6e5a90957cfe6344d06ca27bb0002520170fbcca
Dileep Singh <dileep2008apex@gmail.com>
2015-06-06 22:06:23 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php 
 require('Pusher.php');
 // you get your own app key, secret and id, by creating an account at pusher.com
-$pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
+$pusher = new Pusher('3cf3c7ca0b7957dece97', '89ebd8a6972d5b24befa', 123356);
 
 $user_id = rand(1,4000);
 $user_info = array('name' => $_GET["your_name"]);
